182bd274eca6eb240ebcc56a0f69373e441f9bbc
Eren <eren092003@gmail.com>
2023-06-06 12:58:38 +0200

    finishing project

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -4,7 +4,7 @@
         <div class="bar"></div>
         <div class="status">1 out of 3 questions answered</div>
       </div>
-      <div class="single-question" v-for="question in questions" :key="question.q">
+      <div class="single-question" v-for="(question, qi) in questions" :key="question.q" v-show="questionAnswered">
         <div class="question">{{ question.q }}</div>
         <div class="answers">
           <div class="answer" v-for="answer in question.answers" :key="answer.text">{{ answer.text }}</div>
@@ -17,7 +17,7 @@
 <script>
 export default {
     name: 'Questions',
-    props: ["questions"],
+    props: ["questions", "questionAnswered"],
 }
 </script>
 
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,14 +1,34 @@
 <template>
-    <div class="result">
-      <div class="title">You got sample result 1!</div>
-      <div class="desc">
-        Enter a short description here about the result.
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar" :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"></div>
+      <div class="status">
+        {{ questionsAnswered }} out of {{ questions.length }} questions answered
       </div>
     </div>
+    <transition-group name="fade">
+      <div class="single-question" v-for="(question, qi) in questions" :key="question.q"
+        v-show="questionsAnswered === qi">
+        <div class="question">{{ question.q }}</div>
+        <div class="answers">
+          <div class="answer" v-for="answer in question.answers" :key="answer.text"
+            @click.prevent="selectAnswer(answer.is_correct)">
+            {{ answer.text }}
+          </div>
+        </div>
+      </div>
+    </transition-group>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Result'
-}
+  props: ["questions", "questionsAnswered"],
+  emits: ["question-answered"],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit("question-answered", is_correct);
+    },
+  },
+};
 </script>
